f67ae4602aef0e7563e885270a6ff772d3e5cd84
merms2002 <indense7@GMail.com>
2024-10-21 20:22:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "mrmas bot",
+  botOwner: 'Professor', // مرمس
+  ownerNumber: '917023951514', //01114571370
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
