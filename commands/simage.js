b3d7fd178629b77d096438926b18d27b9a1e63ac
merms2002 <indense7@GMail.com>
2024-10-21 22:57:53 +0300

    Update simage.js

--- commands/simage.js
+++ commands/simage.js
@@ -23,7 +23,7 @@ const convertStickerToImage = async (sock, quotedMessage, chatId) => {
     try {
         const stickerMessage = quotedMessage.stickerMessage;
         if (!stickerMessage) {
-            await sock.sendMessage(chatId, { text: 'Reply to a sticker with .simage to convert it.' });
+            await sock.sendMessage(chatId, { text: 'اعمل منشن للستيكرة اللي عايز تعملها صوره.' });
             return;
         }
 
